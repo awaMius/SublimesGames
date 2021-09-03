6859809ee55b84af63f235e463d7d5a58bf18b42
MiusYT <miguelweinhold2002@hotmail.com>
2021-09-03 22:16:01 +0200

    last update + mini sistema de movimiento

--- Color Out/Assets/Assets/Scripts/PlayerMovement.cs
+++ Color Out/Assets/Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController controller;
-    public float speed = 6f;
+
 
     public float turnSmoothTime = 0.1f;
     float TurnsmoothVelocity;
@@ -15,15 +15,35 @@ public class PlayerMovement : MonoBehaviour
 
     public float gravity = 9.81f;
 
-        public Transform groundCheck;
+    public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
     Vector3 velocity;
     bool isGrounded;
 
-    // Update is called once per frame
+
+    //Anim Controller
+    public float speed = 2f;
+    public float WalkSpeed = 2f;
+    public float TrotSpeed = 4f;
+    public float runningSpeed = 6f;
+    public float RunBlending = 0.1f;
+    [SerializeField] float animspeedfactor;
+    [SerializeField] float maxSpeed;
+    public Animator PlayerAnimator;
+
+    [SerializeField] Vector3 oldDirection;
+
     void Update()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+
+        if (Input.GetKey(KeyCode.Escape)) Application.Quit();
+    }
+    void FixedUpdate()
     {
 
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
@@ -41,6 +61,31 @@ public class PlayerMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
+
+
+            if(direction.x != 0f | direction.z != 0f) oldDirection = direction;
+
+
+
+            if (Input.GetKey(KeyCode.LeftShift) && maxSpeed != WalkSpeed) maxSpeed = runningSpeed; else maxSpeed = TrotSpeed;
+
+
+            if (Input.GetKey(KeyCode.LeftAlt)) { 
+                animspeedfactor = speed / 12;
+                maxSpeed = WalkSpeed;
+            }
+            else animspeedfactor = speed / runningSpeed;
+
+
+
+            if (speed < maxSpeed)
+                speed = speed + RunBlending;
+            else if (speed > maxSpeed) speed = speed - RunBlending; 
+            else if (speed == maxSpeed) speed = maxSpeed;
+
+            PlayerAnimator.SetFloat("speed", animspeedfactor);
+
+
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref TurnsmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
@@ -50,10 +95,31 @@ public class PlayerMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
 
-            velocity.y += gravity * Time.deltaTime;
-
             controller.Move(velocity * Time.deltaTime);
 
+        } else if(direction.magnitude < 0.1f)
+        {
+            animspeedfactor = speed / runningSpeed;
+            if (speed > maxSpeed) speed = speed - RunBlending;
+            PlayerAnimator.SetFloat("speed", animspeedfactor);
+            maxSpeed = 0f;
+
+            //SPEED LOWER
+
+
+
+            float targetAngle = Mathf.Atan2(oldDirection.x, oldDirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref TurnsmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+
+             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+
+
         }
+        controller.Move(velocity * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
     }
 }
