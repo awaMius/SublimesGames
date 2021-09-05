21006ab8726f7264ffa3a42ccba88722fd6f6997
awaMius <miguelweinhold2002@hotmail.com>
2021-09-05 13:54:38 +0200

    new Energy System + Movement Edited a lot

--- Color Out/Assets/Assets/Scripts/PlayerMovement.cs
+++ Color Out/Assets/Assets/Scripts/PlayerMovement.cs
@@ -1,44 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
 
+    //movement + animations
     public CharacterController controller;
-
-
     public float turnSmoothTime = 0.1f;
     float TurnsmoothVelocity;
-
     public Transform cam;
-
     public float gravity = 9.81f;
-
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
-    Vector3 velocity;
-    bool isGrounded;
 
+         //Anim Controller
+         public float WalkSpeed = 2f, TrotSpeed = 4f, runningSpeed = 6f, speed = 0f;
+         public float RunBlending = 0.1f, stopBlendin = 0.3f;
+         public Animator PlayerAnimator;
 
-    //Anim Controller
-    public float speed = 2f;
-    public float WalkSpeed = 2f;
-    public float TrotSpeed = 4f;
-    public float runningSpeed = 6f;
-    public float RunBlending = 0.1f;
-    public float directionCheckerDelay = 0.2f;
-    public Animator PlayerAnimator;
+         [SerializeField] float animspeedfactor;
+         [SerializeField] float maxSpeed;
+         [SerializeField] bool updateDirection, FirstDirectionInput, isGrounded;
+         [SerializeField] float directionCheckerDelay = 0f;
 
-    [SerializeField] float animspeedfactor;
-    [SerializeField] float maxSpeed;
-    [SerializeField] bool updateDirection;
-    [SerializeField] bool FirstDirectionInput;
+         [SerializeField] Vector3 oldDirection, direction, velocity;
 
-    [SerializeField] Vector3 oldDirection;
-    [SerializeField] Vector3 direction;
+    //Energy Controller
+
+    public float maxEnergy = 10f, currentEnergy, EnergyLostPerRunningFrame = 0.1f, EnergyHideBlend = 0.01f, EnergyShowBlend = 0.05f;
+    public Slider EnergyBar;
+    public CanvasGroup EnergyGroup;
+
 
     void Update()
     {
@@ -51,12 +47,36 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
 
+        if(speed < 0)
+        {
+            speed = 0;
+        }
+
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
+
+
+        //energyController
+        if (currentEnergy < maxEnergy && !Input.GetKey(KeyCode.LeftShift))
+            currentEnergy = currentEnergy + EnergyLostPerRunningFrame;
+
+        if (currentEnergy > maxEnergy) { 
+            currentEnergy = maxEnergy;
+        }
+
+        if(currentEnergy == maxEnergy) if (EnergyGroup.alpha > 0f)
+                EnergyGroup.alpha = EnergyGroup.alpha - EnergyHideBlend;
+
+
+        EnergyBar.maxValue = maxEnergy;
+        EnergyBar.value = currentEnergy;
+
+
+        //player controller
 
 
         float horizontal = Input.GetAxisRaw("Horizontal");
@@ -66,7 +86,7 @@ public class PlayerMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
-            if (direction.x != 0f & !updateDirection & FirstDirectionInput| direction.z != 0f & !updateDirection & FirstDirectionInput)
+            if (direction.x != 0f & !updateDirection & FirstDirectionInput | direction.z != 0f & !updateDirection & FirstDirectionInput)
             {
                 Invoke("UpdateDirection", directionCheckerDelay);
                 updateDirection = true;
@@ -78,9 +98,21 @@ public class PlayerMovement : MonoBehaviour
                 FirstDirectionInput = true;
             }
 
+            if (speed > TrotSpeed + 0.1f && currentEnergy > 0f) { 
+                currentEnergy = currentEnergy - EnergyLostPerRunningFrame;
+                EnergyGroup.alpha = EnergyGroup.alpha + EnergyShowBlend;
+            }
 
-            if (Input.GetKey(KeyCode.LeftShift) && maxSpeed != WalkSpeed) maxSpeed = runningSpeed; else maxSpeed = TrotSpeed;
 
+
+            if (Input.GetKey(KeyCode.LeftShift) && maxSpeed != WalkSpeed && currentEnergy > 0f)
+            {
+                maxSpeed = runningSpeed;
+            }
+            else
+            {
+                maxSpeed = TrotSpeed;
+            }
 
             if (Input.GetKey(KeyCode.LeftAlt)) { 
                 maxSpeed = WalkSpeed;
@@ -96,12 +128,9 @@ public class PlayerMovement : MonoBehaviour
 
             PlayerAnimator.SetFloat("speed", animspeedfactor);
 
-
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref TurnsmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
@@ -111,14 +140,17 @@ public class PlayerMovement : MonoBehaviour
         } else if(direction.magnitude < 0.1f)
         {
             animspeedfactor = speed;
-            if (speed > maxSpeed) speed = speed - RunBlending;
+            if (speed > maxSpeed) speed = speed - stopBlendin;
             PlayerAnimator.SetFloat("speed", animspeedfactor);
             maxSpeed = 0f;
 
             //SPEED LOWER
 
-               if(speed <= 0) FirstDirectionInput = false;
-
+            if (speed <= 0)
+            {
+                FirstDirectionInput = false;
+                oldDirection = new Vector3(0, 0, 0);
+            }
             if (speed > 0f)
             {
                 float targetAngle = Mathf.Atan2(oldDirection.x, oldDirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
